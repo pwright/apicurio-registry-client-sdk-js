576ca504cc6cfe4d88930c2e881dea434f88d1bf
Enda Phelan <ephelan@redhat.com>
2022-02-16 15:22:07 +0000

    chore: generate client based on Axios

--- run.ts
+++
@@ -1,9 +0,0 @@
-import { ArtifactsApi } from './src';
-
-const artifactApi = new ArtifactsApi()
-
-artifactApi.listArtifactsInGroup({groupId: "default"}).then((resp) => {
-	console.log(resp);
-}).catch(err => {
-	console.error(err);
-})
---
+++ src/apis/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AdminApi';
+export * from './ArtifactRulesApi';
+export * from './ArtifactsApi';
+export * from './MetadataApi';
+export * from './SystemApi';
+export * from './UsersApi';
+export * from './VersionsApi';
