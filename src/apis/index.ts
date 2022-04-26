e703c2e3f808a5fe85b3b98186ceba1cbcb43573
craicoverflow <craicoverflow@users.noreply.github.com>
2022-04-26 10:22:46 +0000

    chore: update API client

--- src/apis/index.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './AdminApi';
-export * from './ArtifactRulesApi';
-export * from './ArtifactsApi';
-export * from './MetadataApi';
-export * from './SystemApi';
-export * from './UsersApi';
-export * from './VersionsApi';
